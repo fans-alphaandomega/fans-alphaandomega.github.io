17f3b43df811bf8b0e3934d0e8b18fe6aca0dc98
fans-alphaandomega <fans-alphaandomega@users.noreply.github.com>
2020-10-13 16:37:36 +0600

    Mobile

--- js/openPage.js
+++ js/openPage.js
@@ -1,10 +1,8 @@
-<script>
-	function openPage(pageName) {
-		var i;
-		var x = document.getElementsByClassName("page");
-		for (i = 0; i < x.length; i++) {
-			x[i].style.display = "none";  
-		}
-			document.getElementById(pageName).style.display = "block";  
+function openPage(pageName) {
+	var i;
+	var x = document.getElementsByClassName("page");
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";  
 	}
-</script>
+		document.getElementById(pageName).style.display = "block";  
+}
