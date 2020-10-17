3efd97aac966bd977ce6c678cea4004b23b81ccc
fans-alphaandomega <fans-alphaandomega@users.noreply.github.com>
2020-10-17 14:20:48 +0600

    Mobile Update

---
+++ js/showSidebar.js
@@ -0,0 +1,8 @@
+closeSidebar();
+function openSidebar() {
+  document.getElementById("sd").style.display = "block";
+}
+
+function closeSidebar() {
+  document.getElementById("sd").style.display = "none";
+}
