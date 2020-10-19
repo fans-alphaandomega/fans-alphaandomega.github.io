ced37093049d7cf8c22c2566ba263dd8f1cfd3bd
fans-alphaandomega <fans-alphaandomega@users.noreply.github.com>
2020-10-20 00:33:51 +0600

    Update

--- js/changeTheme.js
+++ js/changeTheme.js
@@ -1,39 +1,55 @@
 function darkMode() {
-	var i, j;
+	var i, j, k;
 	// background
-	var w = document.body;
-	w.classList.toggle("dark");
+	var u = document.body;
+	u.classList.toggle("dark");
 	// teal elements
-	var x = document.querySelectorAll('.w3-teal');
-	for (i = 0; i < x.length; i++) {
-		x[i].className = x[i].className.replace(/\bw3-teal\b/g, "w3-indigo");
+	var v = document.querySelectorAll('.w3-teal');
+	for (i = 0; i < v.length; i++) {
+		v[i].className = v[i].className.replace(/\bw3-teal\b/g, "w3-indigo");
+		v[i].classList.toggle("text-yellow");
 	}
+	// switch button
+	var w = document.getElementById('moon');
+	w.style.display = "none";
+	var x = document.getElementById('sun');
+	x.style.display = "block";
 	// buttons
 	var y = document.querySelectorAll('.w3-white');
 	for (j = 0; j < y.length; j++) {
 		y[j].className = y[j].className.replace(/\bw3-white\b/g, "w3-dark-grey");
 	}
 	// side
-	var z = document.getElementById("side");
-	z.style.color = "yellow";
+	var z = document.querySelectorAll('.side');
+	for (k = 0; k < z.length; k++) {
+		z[k].classList.toggle("text-yellow");
+	}	
 }
 
 function lightMode() {
-	var i, j;
+	var i, j, k;
 	// background
-	var w = document.body;
-	w.classList.toggle("dark");
-	// indigo elements
-	var x = document.querySelectorAll('.w3-indigo');
-	for (i = 0; i < x.length; i++) {
-		x[i].className = x[i].className.replace(/\bw3-indigo\b/g, "w3-teal");
+	var u = document.body;
+	u.classList.toggle("dark");
+	// teal elements
+	var v = document.querySelectorAll('.w3-indigo');
+	for (i = 0; i < v.length; i++) {
+		v[i].className = v[i].className.replace(/\bw3-indigo\b/g, "w3-teal");
+		v[i].classList.toggle("text-yellow");
 	}
-	//buttons
+	// switch button
+	var w = document.getElementById('sun');
+	w.style.display = "none";
+	var x = document.getElementById('moon');
+	x.style.display = "block";
+	// buttons
 	var y = document.querySelectorAll('.w3-dark-grey');
 	for (j = 0; j < y.length; j++) {
 		y[j].className = y[j].className.replace(/\bw3-dark-grey\b/g, "w3-white");
 	}
-	//side
-	var z = document.getElementById("side");
-	z.style.color = "white";
+	// side
+	var z = document.querySelectorAll('.side');
+	for (k = 0; k < z.length; k++) {
+		z[k].classList.toggle("text-yellow");
+	}	
 }
