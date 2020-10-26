4f52e341e13111833594792a9fbe50cb8947a41f
fans-alphaandomega <fans-alphaandomega@users.noreply.github.com>
2020-10-27 00:39:24 +0600

    iOS and iPadOS fix

--- js/iDevice.js
+++ js/iDevice.js
@@ -2,7 +2,11 @@ function iDevice() {
 	var isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
 	var x = document.body;
 	if (isIOS) {
+		console.log("Apple device")
 		x.classList.remove("body");
 		x.classList.add("ibody");
 	}
+	else {
+		console.log("Not Apple device");
+	}	
 }
