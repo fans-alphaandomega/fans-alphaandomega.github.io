3218b949a7ffda27ecc5ab7e02d657e204cf0aa5
fans-alphaandomega <fans-alphaandomega@users.noreply.github.com>
2020-11-06 01:45:10 +0600

    Vue.js full integration

--- js/myVue.js
+++ js/myVue.js
@@ -1,5 +1,5 @@
 new Vue ({
-	el: "#main",
+	el: "#wrapper",
 	data: {
 		bio: [
 			{
@@ -58,7 +58,7 @@ new Vue ({
 				about: `Лили - Омега-самка из Западной Стаи, дочь Уинстона и Ив, сестра Кейт, обладающая, пожалуй, самым противоречивым 
 						характером среди главных героев. Волчица также весела и игрива, как и прочие Омеги, но её никогда нельзя заметить в 
 						компании друзей. С возрастом детский задор никуда не пропадает, но, похоже, Лили тщательно скрывает его от всех, кроме 
-						самых близких. Что странно, единственным, с кем Лили запросто становится самой собой - это Гарт, Альфа-волк из Восточной 
+						самых близких. Что странно, единственный, с кем Лили запросто становится самой собой - это Гарт, Альфа-волк из Восточной 
 						Стаи, несостоявшийся "жених" её сестры Кейт. За недолгое время, проведённое вместе, Альфа и Омега очень привязываются друг 
 						к другу, но жестокая реальность сразу же стремится проверить их чувства на прочность...`,
 			},
@@ -79,11 +79,60 @@ new Vue ({
 						"совершенным Альфой", а просто быть самим собой. Взаимопонимание и симпатия Альфы и Омеги перерастает в романтические чувства...`,
 			}
 		],
+		buttonIndex: 0,
 		pageIndex: 0,
 		currentChar: 0,
 		show: false
 	},
 	methods: {
+		darkMode: function() {
+			this.buttonIndex = 1
+			var i, j, k, l
+			var isApple = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream
+			var v = this.$refs["wrapper"]
+			if (isApple) {v.classList.toggle("idark")}
+			else {v.classList.toggle("dark")}
+			var w = this.$el.querySelectorAll(".w3-teal")
+			for (i = 0; i < w.length; i++) {
+				w[i].className = w[i].className.replace("w3-teal", "w3-indigo w3-text-yellow")
+			}
+			var x = this.$el.querySelectorAll(".color")
+			for (j = 0; j < x.length; j++) {
+				x[j].classList.toggle("filter")
+			}	
+			var y = this.$el.querySelectorAll(".w3-dark-grey")
+			for (k = 0; k < y.length; k++) {
+				y[k].className = y[k].className.replace("w3-white", "w3-dark-grey w3-text-orange")
+			}
+			var z = this.$el.querySelectorAll(".side")
+			for (l = 0; l < z.length; l++) {
+				z[l].className = z[l].className.replace("w3-text-white", "w3-text-yellow")
+			}
+		},
+		lightMode: function() {
+			this.buttonIndex = 0
+			var i, j, k, l
+			var isApple = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream
+			var v = this.$refs["wrapper"]
+			if (isApple) {v.classList.toggle("idark")}
+			else {v.classList.toggle("dark")}
+			var w = this.$el.querySelectorAll(".w3-indigo")
+			for (i = 0; i < w.length; i++) {
+				w[i].className = w[i].className.replace("w3-indigo w3-text-yellow", "w3-teal")
+			}
+			var x = this.$el.querySelectorAll(".color")
+			for (j = 0; j < x.length; j++) {
+				x[j].classList.toggle("filter")
+			}	
+			var y = this.$el.querySelectorAll(".w3-dark-grey")
+			for (k = 0; k < y.length; k++) {
+				y[k].className = y[k].className.replace("w3-dark-grey w3-text-orange", "w3-white")
+			}
+			var z = this.$el.querySelectorAll(".side")
+			for (l = 0; l < z.length; l++) {
+				z[l].className = z[l].className.replace("w3-text-yellow", "w3-text-white")
+			}
+		},	
 		openPage: function(index) {
 			this.pageIndex = index
 		},
